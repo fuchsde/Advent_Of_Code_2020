197f8f5b03a4de76faef0086a659b49a7c4fa6ce
Dennis Fuchs <fudennis@gmx.de>
2020-12-05 14:23:08 +0100

    Rework Day_3

--- Day_3/Script.py
+++ Day_3/Script.py
@@ -1,42 +1,10 @@
 import pandas as pd
-import numpy as np
 
-data = pd.read_csv("Data.txt", header=None) 
-route = data.values.tolist()
-
-print("Starting Part One")
-row = 0
-row_increment = 1
-column = 0 
-column_increment = 3
-columns_in_route = len(route[0][0])
-tree = '#'
-counter_trees = 0
-
-for i in range(0, len(route) - 1, row_increment):
-    row = row + row_increment
-    column = column + column_increment
-    current_column = column % columns_in_route
-
-    if route[row][0][current_column] == tree:
-        counter_trees = counter_trees + 1
-
-print(counter_trees)
-
-print("Starting Part Two")
-row_increment_list = [1, 1, 1, 1, 2]
-column_increment_list = [1, 3, 5, 7, 1]
-
-columns_in_route = len(route[0][0])
-tree = '#'
-counter_trees_list = np.zeros(5)
-
-for j in range(0, len(row_increment_list)):
+def count_trees_on_route(route, tree, row_increment, column_increment):
     row = 0
-    row_increment = row_increment_list[j]
     column = 0 
-    column_increment = column_increment_list[j]
     counter_trees = 0
+    columns_in_route = len(route[0][0])
     for i in range(0, len(route) - 1, row_increment):
         row = row + row_increment
         column = column + column_increment
@@ -45,6 +13,39 @@ for j in range(0, len(row_increment_list)):
         if route[row][0][current_column] == tree:
             counter_trees = counter_trees + 1
 
-    counter_trees_list[j] = counter_trees
+    return counter_trees
 
-print(int(counter_trees_list[0] * counter_trees_list[1] * counter_trees_list[2] * counter_trees_list[3] * counter_trees_list[4]))
+def count_trees_part_one(route, tree):
+    row_increment = 1
+    column_increment = 3
+    return count_trees_on_route(route, tree, row_increment, column_increment)
+
+def count_trees_part_two(route, tree):
+    row_increment_list = [1, 1, 1, 1, 2]
+    column_increment_list = [1, 3, 5, 7, 1]
+    counter_trees = 1
+    for j in range(0, len(row_increment_list)):
+        counter_trees = counter_trees * count_trees_on_route(route, tree, row_increment_list[j], column_increment_list[j])
+    return counter_trees
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("Data.txt", header=None) 
+    route = data.values.tolist()
+
+    data = pd.read_csv("Test.txt", header=None) 
+    test_route = data.values.tolist()
+    test_result_first_route = 7
+    test_result_second_route = 336
+
+    tree = '#'
+
+    if count_trees_part_one(test_route, tree) == test_result_first_route:
+        print("Solution Part One: " + str(count_trees_part_one(route, tree)))
+    else:
+        print("Implementation Part One Wrong")
+
+    if count_trees_part_two(test_route, tree) == test_result_second_route:
+        print("Solution Part Two: " + str(count_trees_part_two(route, tree)))
+    else:
+        print("Implementation Part Two Wrong")
