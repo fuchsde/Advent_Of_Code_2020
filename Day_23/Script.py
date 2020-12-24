9bfcdba704f9facc9b31714268a20ce8467b910b
Dennis Fuchs <fudennis@gmx.de>
2020-12-24 17:55:06 +0100

    Rework Day_23

--- Day_23/Script.py
+++ Day_23/Script.py
@@ -1,35 +1,33 @@
-class cup():
-    def __init__(self, id):
-        self.id = id
-        self.next = None
-
 def move_cups(labeling, amount_moves, solving_function):
     current_cup, min_cup, max_cup = 0, min(labeling), max(labeling)
+    amount_cups_to_pick_up = 3
 
     cups_dictionary = dict()
-    for number in labeling:
-        cups_dictionary[number] = cup(number)
-
     for j in range(0, len(labeling) - 1):
-        cups_dictionary[labeling[j]].next = cups_dictionary[labeling[j + 1]]
+        cups_dictionary[labeling[j]] = labeling[j + 1]
 
-    cups_dictionary[labeling[-1]].next = cups_dictionary[labeling[0]]
-    current_cup = cups_dictionary[labeling[0]]
+    cups_dictionary[labeling[-1]] = labeling[0]
+    current_cup = labeling[0]
      
     for _ in range(0, amount_moves):
-        cups_picked_up = [current_cup.next.id, current_cup.next.next.id, current_cup.next.next.next.id]
-        current_cup.next = current_cup.next.next.next.next
+        cups_picked_up = [cups_dictionary[current_cup]]
+        for j in range(0, amount_cups_to_pick_up - 1):
+            cups_picked_up.append(cups_dictionary[cups_picked_up[-1]])
 
-        cup_destination = current_cup.id - 1
+        next_current_cup = cups_dictionary[cups_picked_up[-1]]
+
+        cup_destination = current_cup - 1
         while cup_destination in cups_picked_up or cup_destination < min_cup: 
             if(cup_destination < min_cup):
                 cup_destination = max_cup
             else:
                 cup_destination = cup_destination - 1  
 
-        cups_dictionary[cups_picked_up[2]].next = cups_dictionary[cup_destination].next
-        cups_dictionary[cup_destination].next = cups_dictionary[cups_picked_up[0]]
-        current_cup = current_cup.next
+        cups_dictionary[current_cup] = cups_dictionary[cups_picked_up[-1]]
+        cups_dictionary[cups_picked_up[-1]] = cups_dictionary[cup_destination]
+        cups_dictionary[cup_destination] =  cups_picked_up[0]
+
+        current_cup = next_current_cup
 
     return solving_function(cups_dictionary)
 
@@ -37,7 +35,7 @@ def solving_function_part_one(cups_dictionary):
     labeling_after_1, current_id = "", 1
 
     for _ in range(0, len(labeling) - 1):
-        current_id = cups_dictionary[current_id].next.id
+        current_id = cups_dictionary[current_id]
         labeling_after_1 = labeling_after_1 + str(current_id)
 
     return labeling_after_1
@@ -46,7 +44,7 @@ def solving_function_part_two(cups_dictionary):
     product_two_labels_after_1, current_id = 1, 1
 
     for _ in range(0, 2):
-        current_id = cups_dictionary[current_id].next.id
+        current_id = cups_dictionary[current_id]
         product_two_labels_after_1 = product_two_labels_after_1 * current_id
 
     return product_two_labels_after_1
