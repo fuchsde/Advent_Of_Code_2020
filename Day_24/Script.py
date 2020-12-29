119d741ed08173fcd904078c3beca67d271c1ccf
Dennis Fuchs <fudennis@gmx.de>
2020-12-29 14:40:30 +0100

    Rework Day_24 (Fast Implementation)

--- Day_24/Script.py
+++ Day_24/Script.py
@@ -2,11 +2,10 @@ import copy
 
 def get_amount_black_tiles(tile_list, return_list_black_tiles = False):
     amount_black_tiles = 0
-    tile_coordinates, black_tiles_coordinates = [], []
+    black_tiles_coordinates = {}
 
     for each_tile in tile_list:
         tile_orientation = each_tile
-        tile_dict = {}
         x, y = 0, 0
 
         x = x + (tile_orientation.count("nw") + tile_orientation.count("sw")) * -1
@@ -22,38 +21,25 @@ def get_amount_black_tiles(tile_list, return_list_black_tiles = False):
         x = x + tile_orientation.count("w") * -2
         x = x + tile_orientation.count("e") * 2
 
-        tile_dict['x'] = x
-        tile_dict['y'] = y
-        tile_coordinates.append(tile_dict)
+        key = str(x) + "|" + str(y)
+        if key in black_tiles_coordinates:
+            del black_tiles_coordinates[key]
+        else:
+            black_tiles_coordinates[key] = "Black Tile"
 
-    for each_tile_coordinate in tile_coordinates:
-        if tile_coordinates.count(each_tile_coordinate) == 1:
-            amount_black_tiles = amount_black_tiles + 1
-            black_tiles_coordinates.append(each_tile_coordinate)
-
+    amount_black_tiles = len(black_tiles_coordinates)
+    
     if return_list_black_tiles == False:
         return amount_black_tiles
     else:
         return black_tiles_coordinates
 
-def get_tile_information(x, y, old_black_tile_coordinates):
-    tile = {}
-    tile['x'], tile['y'] = x, y
-     
-    tile_is_black = False
-    if tile in old_black_tile_coordinates:
-        tile_is_black = True
-
-    return tile, tile_is_black
-
-def get_amount_adjacant_black_tiles(x, y, old_black_tile_coordinates):
+def get_amount_adjacant_black_tiles(tile_coordinate, old_black_tile_coordinates):
     adjacant_black_tiles = 0
     delta_coordinates = [[-2, 0], [-1, 1], [1, 1], [2, 0], [1, -1], [-1, -1]]
-
+    x, y = map(int, tile_coordinate.split('|'))
     for each_delta_coordinate in delta_coordinates:
-        adjacant_tile = {}
-        adjacant_tile['x'] = x + each_delta_coordinate[0]
-        adjacant_tile['y'] = y + each_delta_coordinate[1]
+        adjacant_tile = str(x + each_delta_coordinate[0]) + '|' + str(y + each_delta_coordinate[1])
         
         if adjacant_tile in old_black_tile_coordinates:
             adjacant_black_tiles = adjacant_black_tiles + 1
@@ -66,22 +52,21 @@ def get_amount_black_tiles_after_n_days(tile_list, amount_days):
     old_black_tile_coordinates = get_amount_black_tiles(tile_list, True)
     
     for _ in range(0, amount_days):
-        new_black_tile_coordinates = []
+        new_black_tile_coordinates = {}
 
         for each_black_tile in old_black_tile_coordinates:
+            x, y = map(int, each_black_tile.split('|'))
             for each_delta_coordinate in delta_coordinates:
-                tile = {}
-                tile['x'] = each_black_tile['x'] + each_delta_coordinate[0]
-                tile['y'] = each_black_tile['y'] + each_delta_coordinate[1]
+                new_key = str(x + each_delta_coordinate[0]) + '|' + str(y + each_delta_coordinate[1])
 
-                if not tile in new_black_tile_coordinates:
-                    adjacant_black_tiles = get_amount_adjacant_black_tiles(tile['x'], tile['y'], old_black_tile_coordinates)
+                if not new_key in new_black_tile_coordinates:
+                    adjacant_black_tiles = get_amount_adjacant_black_tiles(new_key, old_black_tile_coordinates)
 
                     if adjacant_black_tiles == 2:
-                        new_black_tile_coordinates.append(tile)
-                    elif adjacant_black_tiles == 1 and tile in old_black_tile_coordinates:
-                        new_black_tile_coordinates.append(tile)
-
+                        new_black_tile_coordinates[new_key] = "Black Tile"
+                    elif adjacant_black_tiles == 1 and new_key in old_black_tile_coordinates:
+                        new_black_tile_coordinates[new_key] = "Black Tile"
+                        
         old_black_tile_coordinates = copy.deepcopy(new_black_tile_coordinates)
             
     amount_black_tiles = len(new_black_tile_coordinates)
